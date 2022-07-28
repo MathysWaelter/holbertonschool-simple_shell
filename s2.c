6774b932e791d1717bb172b30aaadd0d37015b4f
rb5014 <5014@holbertonschool.com>
2022-07-28 12:47:14 +0200

    tests

--- s2.c
+++ s2.c
@@ -5,41 +5,36 @@
 #include <sys/wait.h>
 #include <string.h>
 
-
 int main(void)
 {
 	size_t len = 0;
 	int status;
 	pid_t child = 0;
 	char *newarg[] = {NULL, NULL}, *newenv[] = {NULL};
+	char *line = NULL;
 
 
 	while (1)
 	{
-		char *line = NULL;
-
-
+		printf("#Cisfun$ ");
 		child = fork();
 		if (child == 0)
 		{
-			printf("#Cisfun ");
-			if (getline(&line, &len, stdin) != -1)
+			if ((getline(&line, &len, stdin) != -1))
 			{
+
 				newarg[0] = strtok(line, "\n");
 				if (execve(newarg[0], newarg, newenv) == -1)
 				{
 					perror("./shell");
-
 				}
-
+				free(line);
+				line = NULL;
+				return (0);
 			}
-			free(line);
-			return (0);
 		}
 		else
-		{
 			wait(&status);
-		}
 	}
 	return (0);
 }
