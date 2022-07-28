76a5de5286c9bb216c5b86c57112762bff7d8390
rb5014 <5014@holbertonschool.com>
2022-07-28 15:22:28 +0200

    Task 0

--- simple_shell.c
+++ simple_shell.c
@@ -4,7 +4,11 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
-
+/**
+ * main - creates a shell
+ *
+ * Return: Always 0
+ */
 
 int main(void)
 {
@@ -12,34 +16,30 @@ int main(void)
 	int status;
 	pid_t child = 0;
 	char *newarg[] = {NULL, NULL}, *newenv[] = {NULL};
+	char *line = NULL;
+	ssize_t nread;
 
 
-	while (1)
-	{
-		char *line = NULL;
+		write(1, "#Cisfun$ ", 9);
 
-
-		child = fork();
-		if (child == 0)
+		while ((nread = getline(&line, &len, stdin)) != -1)
 		{
-			printf("#Cisfun ");
-			if (getline(&line, &len, stdin) != -1)
+			child = fork();
+			if (child == 0)
 			{
 				newarg[0] = strtok(line, "\n");
 				if (execve(newarg[0], newarg, newenv) == -1)
 				{
 					perror("./shell");
-
 				}
 
 			}
-			free(line);
-			return (0);
+			else
+				free(line);
+				wait(&status);
+
+			write(1, "#Cisfun$ ", 9);
+
 		}
-		else
-		{
-			wait(&status);
-		}
-	}
 	return (0);
 }
