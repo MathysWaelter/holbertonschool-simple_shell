1b0d140fe7b1a14f41400d8fc9de4b5be9397df7
rb5014 <5014@holbertonschool.com>
2022-07-29 14:03:07 +0200

    yeh

--- simple_shell.c
+++ simple_shell.c
@@ -12,7 +12,9 @@ int main(void)
 	pid_t child = 0;
 	char *newarg[120] = {NULL, NULL};
 	ssize_t nread;
-	char *delim = " \n", *token[120], *line = NULL;
+	char *delim = " \n";
+	char *line = NULL;
+	char *token[120];
 
 	while ((nread = getline(&line, &len, stdin)) != -1)
 	{
@@ -21,15 +23,18 @@ int main(void)
 		{
 			token[i] = strtok(line, delim);
 			newarg[0] = strdup(token[i]);
+			write(1, newarg[0], len);
 			while ((token[i] = strtok(NULL, delim)) != NULL)
 			{
 				strcat(newarg[0], " ");
 				strcat(newarg[0], token[i]);
 				i++;
 			}
+			write(1, newarg[0], len);
 			if (newarg[0] == NULL)
 			{
 				free(line);
+				line = NULL;
 				return (0);
 			}
 			if (execve(newarg[0], newarg, environ) == -1)
@@ -42,5 +47,6 @@ int main(void)
 			wait(&status);
 	}
 	free(line);
+	line = NULL;
 	return (0);
 }
