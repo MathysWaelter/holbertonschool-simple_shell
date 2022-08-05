24833b129351cc01149e71fab687498888fa57c2
rb5014 <5014@holbertonschool.com>
2022-08-05 16:28:51 +0200

    which modified to check for path == NULL

--- which.c
+++ which.c
@@ -13,30 +13,33 @@ int _which(char **args, int *status)
 
 	if (strchr(args[0], '/'))
 		return (0);
-	copyenv = strdup(path);
-	lenarg = strlen(args[0]);
-	cmdpath = strdup(args[0]);
-	envNULL = copyenv;
-	while ((token = strtok(envNULL, ":")) != NULL)
+	if (path)
 	{
-		envNULL = NULL;
-		lentok = strlen(token);
-		free(cmdpath);
-		cmdpath = calloc((lentok + lenarg + 2), sizeof(char));
-		cmdpath = strcpy(cmdpath, token);
-		cmdpath = strcat(cmdpath, "/");
-		cmdpath = strcat(cmdpath, args[0]);
-		if (access(cmdpath, F_OK) == 0)
+		copyenv = strdup(path);
+		lenarg = strlen(args[0]);
+		cmdpath = strdup(args[0]);
+		envNULL = copyenv;
+		while ((token = strtok(envNULL, ":")) != NULL)
 		{
-			free(args[0]);
-			args[0] = strdup(cmdpath);
+			envNULL = NULL;
+			lentok = strlen(token);
 			free(cmdpath);
-			free(copyenv);
-			return (0);
+			cmdpath = calloc((lentok + lenarg + 2), sizeof(char));
+			cmdpath = strcpy(cmdpath, token);
+			cmdpath = strcat(cmdpath, "/");
+			cmdpath = strcat(cmdpath, args[0]);
+			if (access(cmdpath, F_OK) == 0)
+			{
+				free(args[0]);
+				args[0] = strdup(cmdpath);
+				free(cmdpath);
+				free(copyenv);
+				return (0);
+			}
 		}
+		free(cmdpath);
+		free(copyenv);
 	}
-	free(cmdpath);
-	free(copyenv);
 	write(STDERR_FILENO, "./hsh: 1: ", 10);
 	write(STDERR_FILENO, args[0], strlen(args[0]));
 	write(STDERR_FILENO, ": not found\n", 12);
